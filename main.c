9679122515220888bf85d4da35d0e118fb874400
Natalia Jimenez Martin <nataliajm115@gmail.com>
2026-01-27 13:39:21 +0100

    update memory initialisation in main

--- main.c
+++ main.c
@@ -2,16 +2,24 @@
 #include <stdlib.h>
 
 int main(void) {
+	FILE* file = fopen("text.txt", "r");
+	
+	Memory *memory = NULL;
+	int st = memInit(&memory);
+	if (!st) {
+		fprintf(stderr, "Not enough memory.\n");
+		fclose(file);
+		exit(0);
+	}
 
-Memory * memory;
-FILE* file = fopen("text.txt", "r");
+	char line[256];
+	while (fgets(line, sizeof(line), file))  {
+		interpretLine(&memory, line);
+		if (code != 0) {
 
-char line[10];
-while (fgets(line, sizeof(line), file))  {
-    interpretLine(&memory, line);
-}
-
-fclose(file);
+	}
+	fclose(file);
+	memFree(memory);
 
 // the errors
 
